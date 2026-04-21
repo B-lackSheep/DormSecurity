47885d99391f67df7c84ea5a3693617760dee2df
B-lackSheep <zhenyakarachun41@mail.ru>
2026-04-21 08:40:02 +0300

    fixed get_session.py

--- get_session.py
+++ get_session.py
@@ -3,7 +3,8 @@ from pyrogram import Client
 from src.config import Config
 
 async def get_session():
-    app = Client("session", api_id=Config.API_ID, api_hash=Config.API_HASH)
+    # Используем существующую сессию sb_userbot
+    app = Client("sb_userbot", api_id=Config.API_ID, api_hash=Config.API_HASH)
     await app.start()
     session_string = await app.export_session_string()
     print(f"SESSION_STRING={session_string}")
